d92ab9c3592712d5498a969db666f9f1d896f6bb
josemanuel <treysitrysi@gmail.com>
2025-09-23 14:09:07 +0100

    Creacion de los archivo manifest y json

--- src/js/app.js
+++ src/js/app.js
@@ -5,28 +5,36 @@ let btnShowPost;
 let btnCancelPost;
 
 //Funciones
-window.addEventListener('load',()=>{
-    main=document.querySelector('#main');
-    modalPost =document.getElementById('modal-post-section');
-    addPost =document.getElementById('btn-upload-post');
-    btnShowPost =document.getElementById('btn-post-submit');
-    btnCancelPost =document.getElementById('btn-post-cancel');
-    addPost.addEventListener('click',ShowPostModal);
-    addPost.addEventListener('click',ClosePostModal);
+window.addEventListener('load', async () => {
+    main = document.querySelector('#main');
+    modalPost = document.getElementById('modal-post-section');
+    addPost = document.getElementById('btn-upload-post');
+    btnShowPost = document.getElementById('btn-post-submit');
+    btnCancelPost = document.getElementById('btn-post-cancel');
+    addPost.addEventListener('click', ShowPostModal);
+    addPost.addEventListener('click', ClosePostModal);
+
+    if ('serviceWorker' in navigator) {
+       const response = await navigator.serviceWorker.register('./sw.js');
+        if (response) {
+            console.log("Servicio worker registro");
+        }
+    }
+
 
 });
 
-const ShowPostModal = () => { 
-main.style.display = 'none';
-modalPost.style.display = 'block';
+const ShowPostModal = () => {
+    main.style.display = 'none';
+    modalPost.style.display = 'block';
 
-setTimeout(()=>{
-    modalPost.style.transform = 'translateY(0)';1
-},1);
+    setTimeout(() => {
+        modalPost.style.transform = 'translateY(0)'; 1
+    }, 1);
 }
 
-const ClosePostModal = () => { 
-main.style.display = 'block';
-modalPost.style.transform = 'translateY(100vh)';
+const ClosePostModal = () => {
+    main.style.display = 'block';
+    modalPost.style.transform = 'translateY(100vh)';
 
 }
